f06418430b3ca87bf8974b1352b23bb9cf6a631e
KRAFT-Foods <34317915+KRAFT-Foods@users.noreply.github.com>
2019-05-02 00:51:21 +0100

    Commit 3 - aligned everything properly, added favorites buttons - next step: separate Men's from Women's and fully implement favorite functionality

--- app/src/main/java/com/example/ali/myapplication/ProductsActivity.java
+++ app/src/main/java/com/example/ali/myapplication/ProductsActivity.java
@@ -1,13 +1,30 @@
 package com.example.ali.myapplication;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ProductsActivity extends AppCompatActivity {
 
+    boolean isEnable=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        final ImageButton ButtonStar = (ImageButton) findViewById(R.id.favorite_button);
+        ButtonStar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isEnable){
+                    ButtonStar.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),android.R.drawable.btn_star_big_off));
+                }else{
+                    ButtonStar.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),android.R.drawable.btn_star_big_on));
+                }
+                isEnable = !isEnable;
+            }
+        });
     }
 }
